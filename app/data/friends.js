6efc31aad4423b46fd0e07fdbc76f0184591bb61
unknown <wroehrman@yahoo.com>
2018-08-11 16:35:55 -0500

    update to all files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Maverick",
+		photo: "https://i.pinimg.com/474x/f8/2e/1d/f82e1d914638bc06d26881223feec0f7--top-gun-movie-cruise-in.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Goose",
+		photo: "https://i.pinimg.com/474x/3b/86/01/3b86011d952bdf8d70880eaa280bf6fa--s-movies-series-movies.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Iceman",
+		photo: "https://i.pinimg.com/474x/07/2d/09/072d09e8d0436c1720be21ab774c7eab--professional-school-sexy-men.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Slider",
+		photo: "https://i.pinimg.com/474x/c6/7b/4b/c67b4b0df72edd78bfde1d7b55553743.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Merlin",
+		photo: "https://i.pinimg.com/474x/e5/7c/1f/e57c1f2ca9dc2fb603ecb88adec61374.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
 require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
 
 
